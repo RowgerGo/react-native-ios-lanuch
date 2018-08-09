ca1f3273102d8e2c1adb9f34bed378b1afa7e58d
RowerGo <php_echo@163.com>
2018-08-09 14:32:50 +0800

    删除多余内容

--- ios/lanuch/ScrollView/ViewController.swift
+++ ios/lanuch/ScrollView/ViewController.swift
@@ -22,27 +22,10 @@ class ViewController: UIViewController,SwiftIntroViewDelegate {
   // SwiftIntroViewDelegate 方法
   func doneButtonClick() {
     self.navigationController?.popViewController(animated: false)
-//    UIView.animate(withDuration: 1, delay: 0, options: .allowAnimatedContent, animations: { () -> Void in
-//      self.introView.alpha = 0
-//    }) { (finished) -> Void in
-//      self.introView.removeFromSuperview()
-//      self.createdMainView()
-//    }
-    
   
   }
   
-  func createdMainView () {
-    let dragView = UIImageView(frame: self.view.frame)
-    dragView.image = UIImage(named: "imageName")
-    dragView.alpha = 0
-    view.addSubview(dragView)
-    UIView.animate(withDuration: 2, delay: 0, options: .allowAnimatedContent, animations: { () -> Void in
-      dragView.alpha = 1
-    }) { (finished) -> Void in
-      
-    }
-  }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
